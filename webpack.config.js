4e6b563f3c48f1ddd3a16a33842258f2ace239c2
MrEnderNasha <53336418+MrEnderNasha@users.noreply.github.com>
2020-10-11 18:57:51 +0700

    Framework start (#4)
    * Finish base framework functional
    
    * Finish base framework functional

--- webpack.config.js
+++ webpack.config.js
@@ -14,14 +14,11 @@ const jsLoaders = () => {
 		{
 			loader: "babel-loader",
 			options: {
-				presets: ['@babel/preset-env']
+				presets: ['@babel/preset-env'],
+				plugins: ['@babel/plugin-proposal-class-properties']				
 			}
 		}
 	];
-
-	if (isDev) {
-		loaders.push('eslint-loader');
-	}
 
 	return loaders;
 }
